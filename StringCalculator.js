23816f539b42e6c79cee6cbc9018cbf4495b6ab8
Vartika_201 <vartu.yadav@gmail.com>
2025-01-16 19:23:57 +0530

    Numbers filter check commit

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,6 +1,18 @@
 class StringCalculator {
   add(numbers) {
     if (!numbers) return 0
+
+    let delimiter = /,|\n/
+    if (numbers.startsWith('//')) {
+      const delimiterMatch = numbers.match(/^\/\/(.+)\n/)
+      delimiter = new RegExp(delimiterMatch[1])
+      numbers = numbers.slice(delimiterMatch[0].length)
+    }
+
+    const nums = numbers
+      .split(delimiter)
+      .map((num) => parseInt(num, 10))
+      .filter((num) => !isNaN(num))
   }
 }
 
